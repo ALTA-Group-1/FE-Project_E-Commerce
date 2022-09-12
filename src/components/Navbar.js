e6351754df6131e7e591eb27957f0e6e27fb3e6e
sandi <adithiasandijosua@gmail.com>
2022-09-12 22:07:59 +0700

    add account

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,10 +36,11 @@ const NavBar = (props) => {
                     className='p-0'>
                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                   </svg></Nav.Link>
-                  <div>
+                  <div className="me-5">
                   <Nav.Link href="#action2" className='p-0'>$ {props.total}</Nav.Link>
                   <Nav.Link href="#action2" className='p-0'>{props.item} item</Nav.Link>
                   </div>
+                  <Nav.Link href="">Account <img src={props.account} width="25" height="25"></img></Nav.Link>
                 </>
               )}
             </Nav>
