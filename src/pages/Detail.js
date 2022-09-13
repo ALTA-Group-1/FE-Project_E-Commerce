87cfcedec47050f20e9df2c3fca73fec6279dd98
doffaarinalhaq <adoffa@gmail.com>
2022-09-13 13:35:06 +0700

    Detail Page

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import '../style/Detail.css';
+import Navbar from '../components/Navbar';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const Detail = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div>Detail
+    <div>
       <div>
-        <Button onClick={() => navigate("/")}>Home</Button>
+        <Navbar />
+        <div>
+          <Container className="container">
+            <Row className="row">
+              <Col md={{ span: 6, offset: 0 }} className="col1">
+                <Card.Img variant="top" className="img" src="https://images.tokopedia.net/img/cache/300-square/product-1/2020/9/2/48125333/48125333_eb395cfa-025e-4c35-b52e-bb128514db19_700_700.webp?ect=4g" alt="gambar" />
+              </Col>
+              <Col md={{ span: 6, offset: 0 }} className="col2">
+                <h4>Product</h4>
+                <p>Rp 123</p>
+                <p>Quantity</p>
+                <h6>Descripton Product</h6>
+                <p>adjaskaa aaaaaaaaa aaaaaaaaaaaaaaaaajhf</p>
+                <Button variant="info" className="btn1">
+                  Add To Cart
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
