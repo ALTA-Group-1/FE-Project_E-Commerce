47526b9ab2f69af4fe3dffcda4cc20d1a956df36
syaanwar26 <sya.anwar26@gmail.com>
2022-09-14 22:41:28 +0700

    update footer

--- src/components/FooterStyles.js
+++ src/components/FooterStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Box = styled.div`
 // padding: 80px 60px;
 background: #f8f9fa;
-position: absolute;
+position: relative;
 bottom: 0;
 width: 100%;
 
