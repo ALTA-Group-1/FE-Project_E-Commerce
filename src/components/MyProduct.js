0cc551e31b099cbd44aa8e9cf87ee079025144c8
syaanwar26 <sya.anwar26@gmail.com>
2022-09-16 05:43:47 +0700

    hit api mystore

--- src/components/MyProduct.js
+++ src/components/MyProduct.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import { Container, Button } from 'react-bootstrap';
+import { useCookies } from 'react-cookie';
 import '../style/MyProduct.css';
+import axios from 'axios';
 
-const MyProduct = () => {
+const MyProduct = (props) => {
+    const cookies = useCookies();
+    var axios = require('axios');
+
+    const data = {
+        "productID": props.id
+    }
+    const config = {
+        headers: { Authorization: `Bearer ${props.token}`},
+    };
+    const url = "http://13.57.49.65/myproducts"
+
+    const addProduct = () => {
+        axios
+            .post(url, data, config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error.response.data);
+            })
+        console.log(props.token);
+    }
   return (
     <div>
         <Container className="content mb-5">
             <div className="content-image">
-                <img src="https://pbs.twimg.com/media/FchLCUcaMAIDzUe?format=jpg&name=small" />
+                <img src={props.image} />
             </div>
             <div className="content-detail">
-                <h5>Product</h5>
-                <h5>Rp 123</h5>
-                <h5>Category</h5>
-                <h5>Quantity</h5>
+                <h5>{props.productname}</h5>
+                <h5>Rp {props.price}</h5>
+                <h5>{props.category}</h5>
                 <h5>Product Description</h5>
-                <p style={{ width: '100%'}}>Aoashi adalah serial manga Jepang yang ditulis dan diilustrasikan oleh Yūgo Kobayashi dan berdasarkan konsep oleh Naohiko Ueno. Ini telah diserialkan di majalah manga seinen Shogakukan Weekly Big Comic Spirits sejak Januari 2015.</p>
+                <p style={{ width: '100%'}}>{props.description}</p>
                 <div className="btnEdit pt-3">
-                    <Button variant="info" style={{marginRight: '20px'}} type="submit">Edit Product</Button>
-                    <Button variant="info" type="submit">Remove Product</Button>
+                    {
+                        props.MyProduct == undefined ?
+                        <>
+                        </> :
+                        <>
+                        <Button variant="info" style={{marginRight: '20px'}} type="submit" onClick={() => addProduct()}>Edit Product</Button>
+                        <Button variant="info">Remove Product</Button>
+                        </>
+                    }
                 </div>
             </div>
         </Container>
--- src/pages/MyStore.js
+++ src/pages/MyStore.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import { Container, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 import MyProduct from '../components/MyProduct';
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
 import '../style/MyProduct.css';
+import axios from 'axios';
 
 const MyStore = () => {
   const navigate = useNavigate();
+  const [cookies, setCookies] = useCookies();
+  const [product, setProduct] = useState();
+  console.log(cookies.token);
+  const config = {
+    headers: { Authorization: `Bearer ${cookies.token}`},
+  };
+  const urlAdd = "http://13.57.49.65/myproducts";
+
+  const getProduct = async () => {
+    await axios
+    .get(urlAdd, config)
+    .then((response) => {
+      setProduct(response.data.data)
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+    });
+  };
+
+  useEffect(() => {
+    getProduct()
+  }, [])
+
   return (
     <div style={{ paddingBottom: '50px' }}>
       <NavBar />
@@ -15,8 +40,22 @@ const MyStore = () => {
         <Container className="titlepro">
           <h1>My Product</h1>
         </Container>
-        <MyProduct />
-        <MyProduct />
+        <div>
+          {
+            product == undefined ?
+            <>
+            </> :
+            <>
+              {
+                product.map((item) => {
+                  return (
+                    <MyProduct image={item.images} name={item.productname} price={item.price} id={item.id} token={cookies.token} add={"y"} />
+                  )
+                })
+              }
+            </>
+          }
+        </div>
       </div>
       <Footer />
     </div>
